680c7603a8d6c6a4be3b2b0e439dfe98b5cbe2fd
srivhima <himanshu.braveheart@gmail.com>
2026-06-18 15:59:13 +0530

    fix mongo ssl

--- server/config/db.py
+++ server/config/db.py
@@ -1,24 +1,34 @@
 import os
+import certifi
 from dotenv import load_dotenv
 from pymongo import MongoClient
 
 load_dotenv()
 
 MONGO_URI = os.getenv("MONGO_URI")
-DB_NAME= os.getenv("DB_NAME", "test_db")
+DB_NAME = os.getenv("DB_NAME", "tutorRag")
 
-client=MongoClient(MONGO_URI)
-db=client[DB_NAME]
+client = MongoClient(
+    MONGO_URI,
+    tls=True,
+    tlsCAFile=certifi.where(),
+    serverSelectionTimeoutMS=30000
+)
+
+# connection test
+client.admin.command("ping")
+
+db = client[DB_NAME]
 
 #user_collection
 users_collection = db["users"]
-
 # document collection
 chunk_collection = db["text"]
-#chat collection
-
+# chat collection
 chat_history_collection = db["chat_history"]
-
 # Quiz collection
-quizzes_collection=db["quizzes"]
-quiz_history=db["history"]
+
+quizzes_collection = db["quizzes"]
+quiz_history = db["history"]
+
+print("MongoDB Connected")
