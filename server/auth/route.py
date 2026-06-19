382b1fa8c6ca4980e534e23b6974e488fdb077d1
srivhima <himanshu.braveheart@gmail.com>
2026-06-19 12:15:18 +0530

    debug login

--- server/auth/route.py
+++ server/auth/route.py
@@ -8,19 +8,65 @@ from .hash_utils import hash_password, verify_password
 router = APIRouter()
 security = HTTPBasic()
 
-def authenticate(credentials:HTTPBasicCredentials=Depends(security)):
-    """Authenticate a user using HTTP Basic Auth"""
-    user= users_collection.find_one({"username":credentials.username})
-    if not user or not verify_password(credentials.password, user.get("password")):
-        raise HTTPException(status_code=401, detail="Invalid username or password")
-    # return credentials.username
-    #if you want to return full details
-    return {
-        "username":user.get("username"),
-        "role": user.get("role"),
-        "grade":user.get("grade"),
-        "user_id":str(user.get("_id"))
-    }
+# def authenticate(credentials:HTTPBasicCredentials=Depends(security)):
+#     """Authenticate a user using HTTP Basic Auth"""
+#     user= users_collection.find_one({"username":credentials.username})
+#     if not user or not verify_password(credentials.password, user.get("password")):
+#         raise HTTPException(status_code=401, detail="Invalid username or password")
+#     # return credentials.username
+#     #if you want to return full details
+#     return {
+#         "username":user.get("username"),
+#         "role": user.get("role"),
+#         "grade":user.get("grade"),
+#         "user_id":str(user.get("_id"))
+#     }
+
+def authenticate(credentials: HTTPBasicCredentials = Depends(security)):
+
+    try:
+        print("Searching:", credentials.username)
+
+        user = users_collection.find_one({
+            "username": credentials.username
+        })
+
+        print("Mongo Result:", user)
+
+        if not user:
+            raise HTTPException(
+                status_code=401,
+                detail="User not found"
+            )
+
+        if not verify_password(
+            credentials.password,
+            user.get("password")
+        ):
+            raise HTTPException(
+                status_code=401,
+                detail="Wrong password"
+            )
+
+        return {
+            "username": user["username"],
+            "role": user["role"],
+            "grade": user["grade"],
+            "user_id": str(user["_id"])
+        }
+
+    except Exception as e:
+        print("LOGIN ERROR:", str(e))
+
+        raise HTTPException(
+            status_code=500,
+            detail=str(e)
+        )
+
+
+
+
+
 
 @router.post("/signup/student")
 def signup_student(req: StudentUser):
